38dbf957fc755845bc424480e2fc209f14c36a3b
Sebastian Martha <sebas.martha@gmail.com>
2025-12-21 17:05:13 -0600

    Se fixeo la logia de auto play del hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -44,27 +44,37 @@ const Hero: React.FC<Props> = ({ images, caption }) => {
       (slider) => {
         let timeout: ReturnType<typeof setTimeout>;
         let mouseOver = false;
+
         function clearNextTimeout() {
           clearTimeout(timeout);
         }
+
         function nextTimeout() {
           clearTimeout(timeout);
           if (mouseOver) return;
+
           timeout = setTimeout(() => {
             slider.next();
           }, 2000);
         }
+
         slider.on("created", () => {
+          // 👉 Si solo hay 1 slide, no aplicar autoplay
+          if (slider.track.details.slides.length <= 1) return;
+
           slider.container.addEventListener("mouseover", () => {
             mouseOver = true;
             clearNextTimeout();
           });
+
           slider.container.addEventListener("mouseout", () => {
             mouseOver = false;
             nextTimeout();
           });
+
           nextTimeout();
         });
+
         slider.on("dragStarted", clearNextTimeout);
         slider.on("animationEnded", nextTimeout);
         slider.on("updated", nextTimeout);
